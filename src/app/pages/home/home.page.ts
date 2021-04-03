b72cf1b40f7ae45b3b74376b948906a8bbca0031
JJunio <junio.araujo01@yahoo.com>
2021-04-03 16:47:16 -0300

    Integração com backend de teste JSONSERVER funcionando

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,13 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  private url: string = "http://localhost:4200/status";
   status: boolean;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.verificarStatus();
+  }
 
-    this.status = false;
+  acionar() {
+    var acionarBomba = { ligada: true }
+    this.http.post(this.url, acionarBomba).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.status = resultado['ligada'];
+      },
+      erro => {
+        if (erro.status == 400) {
+          console.log(erro);
+        }
+      }
+    )
+  }
+
+  desligar() {
+
+    var desligarBomba = { ligada: false }
+    this.http.post(this.url, desligarBomba).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.status = resultado['ligada'];
+      },
+      erro => {
+        if (erro.status == 400) {
+          console.log(erro);
+        }
+      }
+    )
+  }
+
+  verificarStatus() {
+    this.http.get(this.url).subscribe(
+      resultado => {
+        console.log(resultado)
+        this.status = resultado['ligada'];
+      },
+      erro => {
+        if (erro.status == 400) {
+          console.log(erro);
+        }
+      }
+    )
   }
 
 }
